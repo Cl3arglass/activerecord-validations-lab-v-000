8f6e19ca8c9eb5186e68b183df4df65722797738
Holden Altaffer <haltaffer@gmail.com>
2018-08-07 19:20:55 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
-  # validates :title, presence: true
+  validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
-  validates :title, inclusion: { in: %w(/Won't Believe/ /Secret/ /Top [0-9]/ /Guess/) }
+  validates :title, inclusion: { in: %w(/Won't Believe/ /Secret/ /Top [number]/ /Guess/) }
 end
