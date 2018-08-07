b91784102783f8e9ca89c61b92a54ba036e3af29
Holden Altaffer <haltaffer@gmail.com>
2018-08-07 20:05:30 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
   # validates :title, inclusion: { in: %w(Won't Believe Secret Top [0-9]* Guess) }
-  validate :is_clickbait?
+  validate :is_clickbait
 
-  def is_clickbait?
+  def is_clickbait
     if !title.include? "Won't Believe" || !title.include? "Secret" || !title.include? "Top" || !title.include? "Guess"
        errors.add(:title, "not clickbait")
     end
